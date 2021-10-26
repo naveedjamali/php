f452e37c717aa1eef0ab7ff8ee7e5f13fb785ea0
Naveed Jamali <naveedjamali@outlook.com>
2021-10-26 16:22:50 +0500

    changes

--- index.php
+++ index.php
@@ -1,21 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP NFTP</title>
-</head>
+<html>
 <body>
-    <?php
-    $name = "naveed";
-    $name = $name."jamali";
-    echo "Hello $name";
-    
-    $num1 = 2;
-    $num2 = 4;
-    $result = $num2 * $num1;
-    echo "<Br>$num1 X $num2 = $result";
-    ?>
+
+<?php
+echo strlen."My first PHP script!";
+?>
+
 </body>
 </html>
