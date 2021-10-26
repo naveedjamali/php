3287161b629664c33c48df9867ec74522585d974
Naveed Jamali <naveedjamali@outlook.com>
2021-10-26 18:05:39 +0500

    echo vs print part II

--- index.php
+++ index.php
@@ -6,20 +6,10 @@
         </title>
     </head>
 <body>
-<?php 
-
-echo "<H1>Echo Single Argument output </H1>";
-echo "<p>echo prints single or multiple arguments!</p>";
-echo ("Also, echo can also use parenthesis as a function.</Br>");
-echo "Single ", "<B>OR</B> ", " Multiple", " arguments does not affect performance</Br>";
-
-$x = 10;
-$y = 20;
-
-echo "x = ".$x."</Br>";
-echo "y = ".$y."</Br>";
-
-echo $x + $y;
+<?php
+print "<h2>PHP is Fun!</h2>";
+print "Hello world!<br>";
+print "I'm about to learn PHP!";
 ?>
 
 </body>
