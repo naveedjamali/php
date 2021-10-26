b358404dd521dc67e9d395860fc10448be4b0bb1
Naveed Jamali <naveedjamali@outlook.com>
2021-10-26 20:25:51 +0500

    string reverse

--- index.php
+++ index.php
@@ -7,6 +7,12 @@
     </head>
 <body>
 <?php
+
+function br(){
+
+    echo "</Br>";
+}
+
 class Car{
     public $color;
     public $model;
@@ -16,26 +22,28 @@ class Car{
         $this->model = $Model;
     }
     public function message(){
-        return "My car is a ".$this->color." ".$this->model."</BR>";
+        return "My car is a ".$this->color." ".$this->model;
     }
 
 }
 $car;
 $myCar = new Car("Red","Volvo");
-
 echo $myCar->message();
 
 $car2 = new Car("Yellow","BMW");
-echo strlen($car2->message());
-echo "</Br>";
-echo str_word_count($car2->message());
 
-echo "</Br>";
-$car2 = NULL;
 
-echo var_dump($car2)."</Br>";
-echo var_dump($car2)."</Br>";
-echo $car2->message();
+br();
+$msg = $car2->message();
+
+
+$hello = "Hello";
+echo $msg;
+br();
+echo strrev($msg);
+br();
+echo strrev($hello);
+br();
 ?>
 
 </body>
