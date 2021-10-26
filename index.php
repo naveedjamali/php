7ddfb06c58cd377243892ef4cb4838048729e492
Naveed Jamali <naveedjamali@outlook.com>
2021-10-26 18:27:37 +0500

    variable var_dump

--- index.php
+++ index.php
@@ -7,9 +7,38 @@
     </head>
 <body>
 <?php
+
+function br(){
+    echo "<br>";
+}
 print "<h2>PHP is Fun!</h2>";
 print "Hello world!<br>";
+
+br();
+
+br();
+
+br();
 print "I'm about to learn PHP!";
+
+
+$x = 2021;
+$y = "Year";
+
+br();
+echo $x;
+br();
+echo $y;
+br();
+echo var_dump($x);
+br();
+echo var_dump($y);
+
+$z = 21.215487;
+br();
+echo $z;
+br();
+echo var_dump($z);
 ?>
 
 </body>
