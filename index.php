fa573ac41bc15f69bd62d94de49941492a5d0012
Naveed Jamali <naveedjamali@outlook.com>
2021-10-26 16:14:18 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP NFTP</title>
+</head>
+<body>
+    <?php
+    $name = "naveed";
+    $name = $name."jamali";
+    echo "Hello $name";
+    
+    $num1 = 2;
+    $num2 = 4;
+    $result = $num2 * $num1;
+    echo "<Br>$num1 X $num2 = $result";
+    ?>
+</body>
+</html>
